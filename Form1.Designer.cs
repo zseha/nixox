c30537c06456947dbada252d7d2be2314df1eb9a
Zlatko <zseha@hotmail.com>
2012-10-12 22:32:18 +0200

    First commit :)
    Finished forms, logic, and game working.. next network multiplayer,
    better design, sounds, english coments

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class parent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCurrentPlayer = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(57, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(45, 45);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Visible = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(121, 64);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(45, 45);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "X";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Visible = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(180, 64);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(45, 45);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "X";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Visible = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(180, 124);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(45, 45);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "X";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Visible = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(121, 124);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(45, 45);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Visible = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(57, 124);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(45, 45);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "X";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Visible = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(180, 190);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(45, 45);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "X";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Visible = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(121, 190);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(45, 45);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "X";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Visible = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Britannic Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(57, 190);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(45, 45);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "X";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Visible = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(12, 34);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(265, 15);
+            this.lblWelcome.TabIndex = 9;
+            this.lblWelcome.Text = "Welcome! Would you like to play?";
+            // 
+            // btnYes
+            // 
+            this.btnYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYes.Location = new System.Drawing.Point(15, 77);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(262, 30);
+            this.btnYes.TabIndex = 10;
+            this.btnYes.Text = "Yes! Let\'s GO!!!";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(15, 114);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(262, 30);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "No, Exit :(";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCurrentPlayer
+            // 
+            this.lblCurrentPlayer.AutoSize = true;
+            this.lblCurrentPlayer.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentPlayer.Location = new System.Drawing.Point(16, 34);
+            this.lblCurrentPlayer.Name = "lblCurrentPlayer";
+            this.lblCurrentPlayer.Size = new System.Drawing.Size(86, 18);
+            this.lblCurrentPlayer.TabIndex = 12;
+            this.lblCurrentPlayer.Text = "Player: 1";
+            this.lblCurrentPlayer.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuNewGame,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStripMenuNewGame
+            // 
+            this.toolStripMenuNewGame.Name = "toolStripMenuNewGame";
+            this.toolStripMenuNewGame.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuNewGame.Text = "New Game ";
+            this.toolStripMenuNewGame.Click += new System.EventHandler(this.toolStripMenuNewGame_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // parent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblCurrentPlayer);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "parent";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "*nix-ox";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCurrentPlayer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuNewGame;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class parent : Form
+    {
+        bool player = false; //Var. Player oznacava koji igrac igra, 0--prvi igrac, 1--drugi igrac
+        short taken1 = 0; //Var. taken oznacava koje dugme(1-9) je vec odigrano, 0 nije zauzeto, 1 zauzeto sa X, 2 zauzeto sa O
+        short taken2 = 0; //short koristim da smanjim kolicinu koristene memorije
+        short taken3 = 0;
+        short taken4 = 0;
+        short taken5 = 0;
+        short taken6 = 0;
+        short taken7 = 0;
+        short taken8 = 0;
+        short taken9 = 0;
+
+        public parent()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (taken1 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn1.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken1 = 1;
+                }
+                if (player == true)
+                {
+                    taken1 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (taken2 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn2.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken2 = 1;
+                }
+                if (player == true)
+                {
+                    taken2 = 2;
+                }
+                changePlayer();
+            }
+
+            checkState();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (taken3 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn3.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken3 = 1;
+                }
+                if (player == true)
+                {
+                    taken3 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (taken6 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn6.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken6 = 1;
+                }
+                if (player == true)
+                {
+                    taken6 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (taken5 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn5.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken5 = 1;
+                }
+                if (player == true)
+                {
+                    taken5 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (taken4 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn4.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken4 = 1;
+                }
+                if (player == true)
+                {
+                    taken4 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (taken7 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn7.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken7 = 1;
+                }
+                if (player == true)
+                {
+                    taken7 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (taken8 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn8.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken8 = 1;
+                }
+                if (player == true)
+                {
+                    taken8 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (taken9 == 0) // ako polje nije iskoristeno, moze se postaviti
+            {
+                btn9.Text = setBtn();
+                changePlayer();
+                if (player == false)
+                {
+                    taken9 = 1;
+                }
+                if (player == true)
+                {
+                    taken9 = 2;
+                }
+                changePlayer();
+            }
+            checkState();
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            setupForGame();
+        }
+        private void setupForGame()
+        {
+            btn1.Visible = true;
+            btn2.Visible = true;
+            btn3.Visible = true;
+            btn4.Visible = true;
+            btn5.Visible = true;
+            btn6.Visible = true;
+            btn7.Visible = true;
+            btn8.Visible = true;
+            btn9.Visible = true;
+
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            player = false;//postavit na prvog igraca
+
+
+            lblWelcome.Visible = false;
+            btnYes.Visible = false;
+            btnExit.Visible = false;
+            lblCurrentPlayer.Visible = true;
+
+        }
+
+        private void changePlayer()//mjenja trenutnog igraca
+        {
+            if (player == true)
+            {
+                player = false;
+                lblCurrentPlayer.Text = "Player: 2(O)";
+            }
+            else
+            {
+                
+                    player = true;
+                    lblCurrentPlayer.Text = "Player: 1(X)";
+            }
+        }
+        private string setBtn()
+        {
+            if (player == true)
+            {
+                changePlayer();
+                return "O";
+            }
+            else if (player == false)
+            {
+                changePlayer();
+                return "X";
+            }
+            return "";
+        }
+
+        private void checkState()
+        {
+            if (taken1 == 1 && taken2 == 1 && taken3 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken1 == 2 && taken2 == 2 && taken3 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken4 == 1 && taken5 == 1 && taken6 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken4 == 2 && taken5 == 2 && taken6 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken7 == 1 && taken8 == 1 && taken9 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken7 == 2 && taken8 == 2 && taken9 == 2)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            
+
+            /*
+             * 
+             * 
+             * Horizontalno player 1 i 2
+             * 
+             * */
+            if (taken1 == 1 && taken4 == 1 && taken7 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken2 == 1 && taken5 == 1 && taken8 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken3 == 1 && taken6 == 1 && taken9 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+
+
+            if (taken1 == 2 && taken4 == 2 && taken7 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken2 == 2 && taken5 == 2 && taken8 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken3 == 2 && taken6 == 2 && taken9 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+
+
+            /*
+             * 
+             * Vertikalno zavrseno!
+             * 
+             * 
+             * */
+
+            if (taken1 == 1 && taken5 == 1 && taken9 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken3 == 1 && taken5 == 1 && taken7 == 1)
+            {
+                MessageBox.Show("Congrats, player 1(X)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+
+            if (taken1 == 2 && taken5 == 2 && taken9 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+            if (taken3 == 2 && taken5 == 2 && taken7 == 2)
+            {
+                MessageBox.Show("Congrats, player 2(O)!!!\n You WON!", "Yaay!");
+                setMainMenu();
+            }
+
+
+
+        }
+        private void setMainMenu()
+        {
+
+            btn1.Visible = false;
+            btn2.Visible = false;
+            btn3.Visible = false;
+            btn4.Visible = false;
+            btn5.Visible = false;
+            btn6.Visible = false;
+            btn7.Visible = false;
+            btn8.Visible = false;
+            btn9.Visible = false;
+
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            player = false;//postavit na prvog igraca
+
+
+            lblWelcome.Visible = true;
+            btnYes.Visible = true;
+            btnExit.Visible = true;
+
+            lblCurrentPlayer.Visible = false;
+            taken1 = 0; //Var. taken oznacava koje dugme(1-9) je vec odigrano, 0 nije zauzeto, 1 zauzeto sa X, 2 zauzeto sa O
+            taken2 = 0;
+            taken3 = 0;
+            taken4 = 0;
+            taken5 = 0;
+            taken6 = 0;
+            taken7 = 0;
+            taken8 = 0;
+            taken9 = 0;
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripMenuNewGame_Click(object sender, EventArgs e)
+        {
+            setupForGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
